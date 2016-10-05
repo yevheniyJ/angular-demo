b99d5c79aa0c7da31983004515ca29fc362877c8
Yevheniy Oliynyk <Yevheniy.Oliynyk@eleks.com>
2016-10-05 10:54:58 +0300

    Removed not used import

--- src/main/java/org/evgen/ua/AngularDemoApplication.java
+++ src/main/java/org/evgen/ua/AngularDemoApplication.java
@@ -2,7 +2,6 @@ package org.evgen.ua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
