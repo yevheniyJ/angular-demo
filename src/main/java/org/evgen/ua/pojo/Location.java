4f39092be4b6ee574ea63668dd935af41aa430fc
Yevheniy Oliynyk <w8for@mail.ru>
2016-10-05 10:38:32 +0300

    Added lombok to project

--- src/main/java/org/evgen/ua/pojo/Location.java
+++ src/main/java/org/evgen/ua/pojo/Location.java
@@ -1,58 +1,13 @@
 package org.evgen.ua.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Location {
 
-    private String address;
+    private final String address;
 
-    private String city;
-
-    public Location() {}
-
-    public Location(String address, String city) {
-        this.address = address;
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Location location = (Location) o;
-
-        if (address != null ? !address.equals(location.address) : location.address != null) return false;
-        return city != null ? city.equals(location.city) : location.city == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = address != null ? address.hashCode() : 0;
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Location{" +
-                "address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                '}';
-    }
+    private final String city;
 }
--- src/main/java/org/evgen/ua/pojo/Wrapper.java
+++ src/main/java/org/evgen/ua/pojo/Wrapper.java
@@ -1,44 +1,11 @@
 package org.evgen.ua.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Wrapper<T> {
 
-    private T content;
-
-    public Wrapper() {
-    }
-
-    public Wrapper(T content) {
-        this.content = content;
-    }
-
-    public T getContent() {
-        return content;
-    }
-
-    public void setContent(T content) {
-        this.content = content;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Wrapper<?> wrapper = (Wrapper<?>) o;
-
-        return content != null ? content.equals(wrapper.content) : wrapper.content == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return content != null ? content.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Wrapper{" +
-                "content=" + content +
-                '}';
-    }
+    private final T content;
 }
